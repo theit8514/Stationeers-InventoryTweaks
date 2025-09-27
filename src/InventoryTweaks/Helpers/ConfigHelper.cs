3e9f5089a7c5e4bc8a2f9a58a9e6d2b4bf914f42
theit8514 <theit8514@users.noreply.github.com>
2025-09-27 17:02:27 -0400

    Code cleanup

--- src/InventoryTweaks/Helpers/ConfigHelper.cs
+++ src/InventoryTweaks/Helpers/ConfigHelper.cs
@@ -11,21 +11,23 @@ internal static class ConfigHelper
 
     public static class General
     {
-        private const string DescriptionEnableRewriteOpenSlots = """
-                                                                 This will enable or disable the Rewrite Open Slots feature.
-                                                                 If enabled, it will rewrite your save data for Open Slots so that tablets, tools, etc will remain open when loading your save.
+        private const string DescriptionEnableRewriteOpenSlots =
+            """
+            This will enable or disable the Rewrite Open Slots feature.
+            If enabled, it will rewrite your save data for Open Slots so that tablets, tools, etc will remain open when loading your save.
 
-                                                                 This option rewrites the StringHash (an integer value) with the ReferenceId (a long value) of the slot that is open.
-                                                                 When loading it will attempt to open the windows matching the StringHash first, then the ReferenceId.
-                                                                 There should not be a problem with loading a save that has been modified this way, as Stationeers ignores unknown Open Slot StringHash values.
-                                                                 """;
+            This option rewrites the StringHash (an integer value) with the ReferenceId (a long value) of the slot that is open.
+            When loading it will attempt to open the windows matching the StringHash first, then the ReferenceId.
+            There should not be a problem with loading a save that has been modified this way, as Stationeers ignores unknown Open Slot StringHash values.
+            """;
 
-        private const string DescriptionEnableLockedSlots = """
-                                                            This will enable or disable the save feature of Locked Slots.
-                                                            If enabled, this will save the locked slots into a new InventoryTweaks.xml file in your game save.
+        private const string DescriptionEnableLockedSlots =
+            """
+            This will enable or disable the save feature of Locked Slots.
+            If enabled, this will save the locked slots into a new InventoryTweaks.xml file in your game save.
 
-                                                            This does not modify the existing world save, only creates a new file in the world folder as the game is saving.
-                                                            """;
+            This does not modify the existing world save, only creates a new file in the world folder as the game is saving.
+            """;
 
         private static ConfigEntry<bool> _configEnableRewriteOpenSlots;
         private static ConfigEntry<bool> _configEnableSaveLockedSlots;
